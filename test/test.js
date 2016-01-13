08ea83a143567c9c5e01b9003951f0d9d61aa672
Boris Diakur <contact@borisdiakur.com>
2016-01-13 14:19:26 +0100

    Upgrade to 1.0.0 due to lodash upgrade to v4.0.0

--- test/test.js
+++ test/test.js
@@ -2,11 +2,7 @@
 /* global describe, beforeEach, it */
 /* jshint unused:vars */
 
-var assert = require('assert'),
-    _ = require('lodash'),
-    n_ = require('../lib/n_'),
-    line = n_.rli._events.line,
-    result = null;
+var assert = require('assert'), _ = require('lodash'), n_ = require('../lib/n_'), line = n_.rli._events.line, result = null;
 
 n_.writer = _.wrap(n_.writer, function (writer, obj) {
     result = obj;
@@ -22,14 +18,19 @@ describe('n_', function () {
     describe('default special var', function () {
         it('should evaluate multiline input', function (done) {
             line('var users = [');
-            line('    { user: "barney", age: 36 },');
-            line('    { user: "fred",   age: 40 },');
-            line('    { user: "barney", age: 26 },');
-            line('    { user: "fred",   age: 30 }');
+            line('    { "user": "fred",   "age": 48 },');
+            line('    { "user": "barney", "age": 36 },');
+            line('    { "user": "fred",   "age": 42 },');
+            line('    { "user": "barney", "age": 34 }');
             line('];');
             assert.equal(result, undefined);
-            line('_.map(_.sortByAll(users, ["user", "age"]), _.values);');
-            assert.deepEqual(result, [['barney', 26], ['barney', 36], ['fred', 30], ['fred', 40]]);
+            line('_.sortBy(users, function(o) { return o.user; });');
+            assert.deepEqual(result, [
+                { user: 'barney', age: 36 },
+                { user: 'barney', age: 34 },
+                { user: 'fred', age: 48 },
+                { user: 'fred', age: 42 }
+            ]);
             done();
         });
         it('should evaluate simple input', function (done) {
@@ -39,10 +40,18 @@ describe('n_', function () {
         });
         it('should evaluate multiple lodash method calls', function (done) {
             line('_.compact([1, 2, false, 4])');
-            assert.deepEqual(result, [1, 2, 4]);
+            assert.deepEqual(result, [
+                1,
+                2,
+                4
+            ]);
             result = null;
             line('_.compact([1, false, 3, 4])');
-            assert.deepEqual(result, [1, 3, 4]);
+            assert.deepEqual(result, [
+                1,
+                3,
+                4
+            ]);
             done();
         });
         it('should evaluate with built in libs', function (done) {
@@ -100,7 +109,10 @@ describe('n_', function () {
             line('qux');
             assert.equal(result, 'abc');
             line('_.compact([1, false, 3, null])');
-            assert.deepEqual(result, [1, 3]);
+            assert.deepEqual(result, [
+                1,
+                3
+            ]);
             done();
         });
     });
